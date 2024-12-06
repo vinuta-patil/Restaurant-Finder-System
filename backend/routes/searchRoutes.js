27c089b04bc9db1582eb1a8fd66a0caa3051d9a5
K Paavani <73386869+paavani019@users.noreply.github.com>
2024-12-06 12:14:26 -0800

    Update searchRoutes.js

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/searchController');
 
+
+
 router.post('/search', searchController.searchRestaurants);
 
 module.exports = router;
