0b5c285361826a8b8ffced83beb7e68e0383a5f0
Golla Ajay Kumar <84775853+AJAYKUMARGOLLA117@users.noreply.github.com>
2024-12-06 12:20:18 -0800

    Update Both.js

--- backend/models/Both.js
+++ backend/models/Both.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 const restaurantSchema = new mongoose.Schema({
     name: { type: String, required: true },
     address: { type: String, required: true },
