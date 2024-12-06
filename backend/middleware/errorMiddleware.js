1276df72890b458fac68d1e0ce9124b36d9e6122
K Paavani <73386869+paavani019@users.noreply.github.com>
2024-12-06 11:44:54 -0800

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -2,6 +2,7 @@ exports.notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
+  
 };
 
 exports.errorHandler = (err, req, res, next) => {
