7317c466e41a481b44b429e7edc41ea2ab874b67
vinutapatil472 <vinuta.patil@sjsu.edu>
2024-12-06 14:36:13 -0800

    Update placesController.js

--- backend/controllers/placesController.js
+++ backend/controllers/placesController.js
@@ -5,8 +5,6 @@ exports.fetchRestaurants = async (req, res) => {
   const { location, radius = 5000, type = 'restaurant' } = req.body;
   const apiKey = process.env.GOOGLE_PLACES_API_KEY;
 
-  
-
   try {
     if (!location) {
       return res.status(400).json({ error: 'Location is required.' });
